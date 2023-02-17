142a6ec1adb041139ddfb79ecf1a1ca6733252f8
Jessy <jes.baer@gmail.com>
2023-02-17 14:36:53 +0100

    feat: replace bootstrap by tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./_posts/*.md",
+    "./index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
