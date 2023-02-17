205901ae733afa88d8a1a5a169c4a8bd164b93a6
Jessy <jes.baer@gmail.com>
2023-02-17 17:29:12 +0100

    feat: add brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,22 @@ module.exports = {
     "./index.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          50: "#EFF8FE",
+          100: "#DDEFFC",
+          200: "#B9DFF9",
+          300: "#83C6F5",
+          400: "#22d3ee",
+          500: "#3E8ABF",
+          600: "#236FA4",
+          700: "#0F5B90",
+          800: "#045085",
+          900: "#004677",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
